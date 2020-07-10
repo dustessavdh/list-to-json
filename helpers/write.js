4845d41f958fc1ee042c09c1ceb1b532cbd11252
Thijs <thijsvdh12@gmail.com>
2020-07-10 23:44:34 +0200

    added new function

---
+++ helpers/write.js
@@ -0,0 +1,15 @@
+const fs = require('fs');   
+
+exports.writeToJson = (newColors) => {
+    let fs = require('fs');
+    fs.readFile("../models/kleuren.json", "utf8", function readFileCallback(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            obj = JSON.parse(data);
+            obj.gamma.colors.push(newColors.colors);
+            var json = JSON.stringify(obj);
+            fs.writeFile("kleuren.json", json, "utf8", callback);
+        }
+    });
+}
