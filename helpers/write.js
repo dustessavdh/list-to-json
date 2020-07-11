296b128ea6d3d7daeacdbc70ae0d008484038f00
Thijs <thijsvdh12@gmail.com>
2020-07-11 19:51:23 +0200

    added return

--- helpers/write.js
+++ helpers/write.js
@@ -5,11 +5,13 @@ exports.writeToJson = (newColors) => {
     fs.readFile("../models/kleuren.json", "utf8", function readFileCallback(err, data) {
         if (err) {
             console.log(err);
+            return "error";
         } else {
             obj = JSON.parse(data);
             obj.gamma.colors.push(newColors.colors);
             var json = JSON.stringify(obj);
             fs.writeFile("../models/kleuren.json", json, "utf8", callback);
+            return "success";
         }
     });
 }
