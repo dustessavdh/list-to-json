92bfe0c048e97dddbc911d34b57b471a95f9f1b9
Thijs <thijsvdh12@gmail.com>
2020-08-12 23:48:41 +0200

    cleanup

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,22 @@ router.get('/', (req, res, next) => {
 });
 
 // On a post request try to save the data
-router.post('/save', (req, res, next) => {
-  //TODO clean this up. maybe move it to a function so it's prettier
-  try {
-    data = JSON.parse(req.body.data);
-  } catch (err) {
-    console.log(err);
-    res.status(400).send('This is not json!');
-  }
+// router.post('/save', (req, res, next) => {
+//   //TODO clean this up. maybe move it to a function so it's prettier
+//   try {
+//     data = JSON.parse(req.body.data);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(400).send('This is not json!');
+//   }
 
-  const writeSuccess = write.writeToJson(data);
+//   const writeSuccess = write.writeToJson(data);
 
-  if (writeSuccess === 'success') {
-    res.send('POST request was good!');
-  } else {
-    res.status(500).send('There occured an error while trying to save the data');
-  }
-});
+//   if (writeSuccess === 'success') {
+//     res.send('POST request was good!');
+//   } else {
+//     res.status(500).send('There occured an error while trying to save the data');
+//   }
+// });
 
 module.exports = router;
