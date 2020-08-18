fdc82cf91f792faeb32d768717bfe7132a4f4344
Thijs <thijsvdh12@gmail.com>
2020-08-18 02:05:45 +0200

    added sessions and flash

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session')
+var flash = require('connect-flash');
 var logger = require('morgan');
 
 // routers
@@ -18,7 +20,14 @@ app.set('view engine', 'hjs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('stuff'));
+app.use(session({
+  secret: 'stuff',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}));
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
