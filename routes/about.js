713dee55593dfa419fbc27b8865e98b86e3c0eeb
Thijs <thijsvdh12@gmail.com>
2020-08-19 00:24:13 +0200

    changed vars to consts and changed some functions to lamdas

--- routes/about.js
+++ routes/about.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET about page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('layout', {
       title: 'About',
       partials: {content: 'about/about'}
